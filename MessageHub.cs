a502d183937e1ff89be80e9793f6dd56142744b1
swislockikacper <k.swislocki96@gmail.com>
2019-03-16 21:20:23 +0100

    Initial commit

---
+++ MessageHub.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace SignalR.NetCore.SignalR
+{
+    public class MessageHub : Hub
+    {
+        public async Task SendMessage(string userName, string content)
+            => await Clients.All.SendAsync("ReceiveMessage", userName, content);
+    }
+}
