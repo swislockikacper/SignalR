6abe3349a9e4993fef934f3cfe0b95704a7f1256
swislockikacper <k.swislocki96@gmail.com>
2019-05-07 16:27:36 +0200

    repaired gitignore

---
+++ wwwroot/js/signalR.js
@@ -0,0 +1,50 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/messageHub").build();
+
+document.getElementById("sendBtn").disabled = true;
+
+connection.on("ReceiveMessage", function (userName, content) {
+    var table = document.getElementById("messagesTable");
+    var row = document.getElementById("messagesTable").insertRow();
+
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+
+    cell1.innerHTML = userName;
+    cell2.innerHTML = content;
+});
+
+connection.start().then(function () {
+    document.getElementById("sendBtn").disabled = false;
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+document.getElementById("sendBtn").addEventListener("click", function (event) {
+    var userName = document.getElementById("userName").value;
+    var content = document.getElementById("content").value;
+    connection.invoke("SendMessage", userName, content).catch(function (err) {
+        return console.error(err.toString());
+    });
+
+    post();
+
+    event.preventDefault();
+});
+
+function post() {
+    var message = {
+        UserName: document.getElementById("userName").value,
+        Content: document.getElementById("content").value
+    };
+
+    $.ajax({
+        type: "POST",
+        url: "/api/Message/Post",
+        data: message
+    });
+
+    document.getElementById("userName").value = "";
+    document.getElementById("content").value = "";
+}
